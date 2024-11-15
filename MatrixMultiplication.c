a18c9d4e16d81fdf30da7317c7d24421554ee5db
Koushikkd07 <152011246+Koushikkd07@users.noreply.github.com>
2024-11-15 20:00:26 +0530

    Update MatrixMultiplication.c

--- MatrixMultiplication.c
+++ MatrixMultiplication.c
@@ -45,7 +45,7 @@ int main(){
     }  
 }
 void multiply(int arr1[100][100],int arr2[100][100], int row1, int column1, int column2){
-    int result[100][100];
+    int result[100][100]={0};
     for (int i = 0; i < row1; i++){
         for (int j = 0; j < column2; j++){
            for (int k = 0; k < column1; k++)
